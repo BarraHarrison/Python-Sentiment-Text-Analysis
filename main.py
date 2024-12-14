7f244de6a5fd22ba62b7931253a821d7ca42a9d4
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2024-12-14 16:27:17 +0900

    Neutral Wikipedia Article

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 # Sentiment Text Analysis using Python
 from textblob import TextBlob
 from newspaper import Article
+
+url = 'https://en.wikipedia.org/wiki/Mathematics'
+article = Article(url)
+
+article.download()
+article.parse()
+article.nlp()
+
+text = article.text
+print(text)
